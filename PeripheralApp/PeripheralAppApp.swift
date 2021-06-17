d65392ca6bf03c3666d58aa122b8d5b276060af8
Tom <tomhoag@Gmail.com>
2021-06-17 10:35:25 -0400

    Initial Commit

---
+++ PeripheralApp/PeripheralAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeripheralAppApp.swift
+//  PeripheralApp
+//
+//  Created by Tom on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct PeripheralAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
